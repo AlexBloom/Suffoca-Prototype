428202e2e1823d3f1424e946b129c94548f6df8f
Alex Bloom <alexbloomdesign@gmail.com>
2015-01-15 18:17:42 -0500

    Post Tweaks

--- post.php
+++ post.php
@@ -4,9 +4,6 @@
 <article class="post">
     <header>
 		<img src="img/content/adventures/la_01.jpg" alt=""/>
-        <ul class="categories"> 
-            <li> <a class="category-link" href="category">Category</a> </li>
-        </ul>
 		<div class="post-title">
 	        <h1>Los Angeles</h1>
 	        <h2>Article Subtitle</h2>
@@ -14,9 +11,15 @@
     </header>
 	<aside class="meta">
         <div class="author"> 
-			<p><img src="img/content/contributors/boyce.jpg" alt="Boyce" />
-			Posted by <a class="author-link" href="/author-namely">Boyce</a></p> 
+			<p>
+				<img src="img/content/contributors/boyce.jpg" alt="Boyce" />
+			By <a class="author-link" href="/boyce">Boyce</a>
+		    in <a class="category-link" href="category">Adventures</a>
+			</p> 
 		</div>
+        <!-- <ul class="categories">
+            <li>  </li>
+        </ul> -->
 	</aside>
     <section>
 		<img src="img/content/adventures/post/6K3A1165.jpg" alt="6K3A1165">
@@ -25,40 +28,43 @@
 		<img src="img/content/adventures/post/6K3A8666.jpg" alt="6K3A8666">
         
 		<blockquote> This was a truly awesome summer adventure, I highly recommend taking a bunch of your friends and taking it to the hills. </blockquote>
-        
+        		
+		<iframe width="1020" height="574" src="//www.youtube.com/embed/JTzae0fPzzM" frameborder="0" allowfullscreen></iframe>
+		</section>	
+		<section class="full-bleed">
 		<img src="img/content/adventures/post/6K3A1584.jpg" alt="6K3A1584">
-		
-		<iframe width="1020" height="574" src="//www.youtube.com/embed/JTzae0fPzzM" frameborder="0" allowfullscreen>
-			
-		</iframe>
-		
+		</section>
+		<section>
 		<p> An amazing installation called Spectra, by Japanese artist Ryoji Ikeda was put up next to the House of Lords to commemorate those in world war 2. The visuals topped with the composed sounds, was a truly body tingling exsperience , it was hard not to feel something when gazing at the huge beams of light connecting us from the ground to the skys.</p>
+		</section>
 		
-		<img src="img/content/adventures/post/6K3A1889.jpg" alt="6K3A1889">
-
-		<img src="img/content/adventures/post/6K3A0960.jpg"  alt="6K3A0960">
-		
-		
-		
+		<section class="full-bleed">
+			<img class="half" src="img/content/adventures/post/6K3A1889.jpg" alt="6K3A1889">
+			<!-- <img class="half" src="img/content/adventures/post/6K3A0960.jpg"  alt="6K3A0960"> -->
+			<img class="half" src="img/content/adventures/post/6K3A1671.jpg" alt="6K3A1671">
+			
+			<img class="half" src="img/content/adventures/post/6K3A1632-Edit.jpg" alt="6K3A1632 Edit">
+			<img class="half" src="img/content/adventures/post/6K3A1816.jpg" alt="6K3A1816">
+		</section>
+		<section>
 		<img src="img/content/adventures/post/6K3A1019.jpg">
         <p>Paragraphs of text</p>
         <figure>
             <img src="img/content/adventures/post/6K3A1448.jpg" alt="6K3A1448">
-            <figcaption> Hiding in the cacti. </figcaption>
+            <figcaption> Hiding in the cacti. This could be a caption style for when you want to make notes about a particular image that don't fall within the normal importance of paragraphs. </figcaption>
         </figure>
         <p>Thirteen of us decided it was time to take a trip together to the south of Spain, to let loose and have some long hangouts with good food and even better company.</p>
         <figure>
-            <blockquote> If you don't build your dream, someone will hire you to help build theirs. </blockquote>
+            <blockquote> If you don't build your dream, someone will hire you to help build theirs. Quote inside of a figure for when you want to share the author name. </blockquote>
             <figcaption> — quote author. </figcaption>
         </figure>
-		<img src="img/content/adventures/post/6K3A1584.jpg" alt="6K3A1584">
     </section>
     <footer>
-        <ul class="tags">
-            <li><a href="#"> tag </a> </li>
-            <li><a href="#"> tag </a> </li>
-            <li><a href="#"> tag </a> </li>
-        </ul>        
+      <ul class="tags">
+          <li><a href="#"> tag </a> </li>
+          <li><a href="#"> tag </a> </li>
+          <li><a href="#"> tag </a> </li>
+      </ul>     
     </footer>
 </article>
 <div class="pagination">
