2c7884eff2a6d2cb6f579ed5ae05a00799873490
Alex Bloom <alexbloomdesign@gmail.com>
2015-02-16 18:18:39 -0500

    Shop Layout Re-Vamped.
    Floating Header, Split @widescreen
    Horizontal Filters
    Re-Work of Grid

--- shop.php
+++ shop.php
@@ -2,71 +2,85 @@
 <?php include_once'inc/header.inc' ?>
 
 
-	<header id="shop-intro" class="full-width" style="background-image:url(img/content/lookbook/hitch_01.jpg)">		
+	<header id="shop-header" style="background-image:url(img/content/lookbook/hitch_01.jpg)">		 
 		<section>
-			<h1>Spring 2015 Lookbook</h1>
-            <button>View</button>
-			<!-- <p>{Vimeo Embed}</p>  -->
+			<h1 class="title">Spring 2015 Lookbook</h1>
+            <h2 class="subtitle"> <button>View</button> </h2>
+			
 		</section>
-        
-	</header>     
-	
+	</header>  
+	<h6 class="logo-subtitle"> SHOP </h6>
 	<dl class="shop-categories">
 		<dt> FILTER </dt>
 		<dd class="filter all"> All </dd>
-		<dd class="filter mens"> Men's 
-            <ul class="subcat mens-sub">
-                <li>Shirts</li>
-                <li>Sweatshirts</li>
-                <li>Hats</li>
-            </ul>
-        </dd>
-		<dd class="filter womens"> Women's 
-            <ul class="subcat womens-sub">
-                <li>Shirts</li>
-                <li>Sweatshirts</li>
-                <li>Hats</li>
-            </ul>
-        </dd>
-		<dd class="filter collectibles"> Collectibles </dd>
-		<dd class="filter prints"> Prints </dd>
+		<dd class="filter shortsleeve"> Shortsleeve  </dd>
+		<dd class="filter longsleeve"> Longsleeve </dd>
+		<dd class="filter misc"> Misc </dd>
+		<dd class="filter art"> Art </dd>
 	</dl>
 
     <ul class="products">
         <li class="product">
            <a href="" class="permalink">
-               <img src="img/content/products/Beanie.jpg" alt="Product Caption">
+              <img src="img/content/products/all/Beanie_Suffoca-400x400.jpg" alt="Beanie Suffoca 400x400">
                <h6> Product Name </h6>
+			   <p class="price"> £15.00 </p>
            </a>
         </li>
         <li class="product">
            <a href="" class="permalink">
-               <img src="img/content/products/Black_T_.jpg" alt="Product Caption">
+               <img src="img/content/products/all/Stickerset-400x400.jpg"  alt="Stickerset 400x400">
                <h6> Product Name </h6>
+			   <p class="price"> £5.00 </p>
            </a>
         </li>
         <li class="product">
            <a href="" class="permalink">
-               <img src="img/content/products/LONGT_.jpg" alt="Product Caption">
+               <img src="img/content/products/all/Suffoca_Bean_Flannel-400x400.jpg"  alt="Suffoca Bean Flannel 400x400">
                <h6> Product Name </h6>
+			   <p class="price"> £32.00 </p>
            </a>
         </li>
         <li class="product">
            <a href="" class="permalink">
-               <img src="img/content/products/Hoodie_01.jpg" alt="Product Caption">
+               <img src="img/content/products/all/Suffoca_BirdFullMove-400x400.gif"  alt="Suffoca BirdFullMove 400x400">
                <h6> Product Name </h6>
+			   <p class="price"> £22.00 </p>
            </a>
         </li>
         <li class="product">
            <a href="" class="permalink">
-               <img src="img/content/products/Crew.jpg" alt="Product Caption">
+               <img src="img/content/products/all/Suffoca_Clubhouse_Full-400x400.gif" alt="Suffoca Clubhouse Full 400x400">
                <h6> Product Name </h6>
+			   <p class="price"> £22.00 </p>
            </a>
         </li>
         <li class="product">
            <a href="" class="permalink">
-               <img src="img/content/products/Stickerset-400x400.jpg" alt="Product Caption">
+               <img src="img/content/products/all/Suffoca_Crew_Type_Full-400x400.gif" alt="Suffoca Crew Type Full 400x400">
                <h6> Product Name </h6>
+			   <p class="price"> £28.00 </p>
+           </a>
+        </li>
+        <li class="product">
+           <a href="" class="permalink">
+			   <img src="img/content/products/all/Suffoca_Script_Front-400x400.jpg"  alt="Suffoca Script Front 400x400">
+               <h6> Product Name </h6>
+			   <p class="price"> £16.00 </p>
+           </a>
+        </li>
+        <li class="product">
+           <a href="" class="permalink">
+			   <img src="img/content/products/all/Suffoca_Skull-400x400.gif" alt="Suffoca Skull 400x400">
+               <h6> Product Name </h6>
+			   <p class="price"> £15.00 </p>
+           </a>
+        </li>
+        <li class="product">
+           <a href="" class="permalink">
+               <img src="img/content/products/all/Suffoca_Strike_Full-400x400.gif" alt="Suffoca Strike Full 400x400">
+               <h6> Product Name </h6>
+			   <p class="price"> £15.00 </p>
            </a>
         </li>
     </ul>
